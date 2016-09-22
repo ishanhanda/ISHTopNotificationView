746e065430dfe0448e26f0ffcf80f27839b6e341
Ishan Handa <ishanhanda@Ishans-MacBook-Pro.local>
2016-09-22 01:25:17 -0400

    Addin Demo

---
+++ ISHTopNotificationViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ISHTopNotificationViewDemo
+//
+//  Created by Ishan Handa on 22/09/16.
+//  Copyright © 2016 Ishan Handa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
